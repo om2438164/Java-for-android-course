538c267d66ecad5a3b88054a7976608e28cc5144
Om Patil <117569343+om2438164@users.noreply.github.com>
2023-06-01 00:43:46 +0530

    Update Tasks of week-2.java

--- Tasks of week-2.java
+++ Tasks of week-2.java
@@ -1,51 +1,37 @@
-package mooc.vandy.java4android.diamonds.logic;
+#The Birthday problem - auto graded#
+answer:
+ public double calculate(int size, int count) {
+        // TODO -- add your code here
+        int dupcount=0;
+        Random r = new Random();
+        for(int i=0;i<count;i++)
+        {
+            int arr[]=new int[365];
+            r.setSeed(i+1);
+            for(int j=0;j<size;j++)
+            {
+                int n=r.nextInt(365);
+                arr[n]++;
+                if(arr[n]>=2) {
+                    dupcount++;
+                    break;
+                }
+            }
+        }
+        return dupcount*100.0/count;
 
-import mooc.vandy.java4android.diamonds.ui.OutputInterface;
-
-/**
- * This is where the logic of this App is centralized for this assignment.
- * <p>
- * The assignments are designed this way to simplify your early
- * Android interactions.  Designing the assignments this way allows
- * you to first learn key 'Java' features without having to beforehand
- * learn the complexities of Android.
- */
-public class Logic
-       implements LogicInterface {
-    /**
-     * This is a String to be used in Logging (if/when you decide you
-     * need it for debugging).
-     */
-    public static final String TAG = Logic.class.getName();
-
-    /**
-     * This is the variable that stores our OutputInterface instance.
-     * <p>
-     * This is how we will interact with the User Interface [MainActivity.java].
-     * <p>
-     * It is called 'out' because it is where we 'out-put' our
-     * results. (It is also the 'in-put' from where we get values
-     * from, but it only needs 1 name, and 'out' is good enough).
-     */
-    private OutputInterface mOut;
-
-    /**
-     * This is the constructor of this class.
-     * <p>
-     * It assigns the passed in [MainActivity] instance (which
-     * implements [OutputInterface]) to 'out'.
-     */
-    public Logic(OutputInterface out){
-        mOut = out;
+    }
+        
     }
 
-    /**
-     * This is the method that will (eventually) get called when the
-     * on-screen button labeled 'Process...' is pressed.
-     */
-    public void process(int size) {
-        // TODO -- add your code here
 
+    // TODO -- add your helper methods here
+    
+
+# Drawing ASCII Art - Auto Graded #
+answer :
+public void process(int size) {
+        // TODO -- add your code here
         int height = size * 2 + 1;
         int width = size * 2 + 2;
         int accumulator = -(size+1);
@@ -65,9 +51,11 @@ public class Logic
             }
             mOut.print("\n");
         }
+        
     }
 
     // TODO -- add any helper methods here
+
     public void drawDiamond(int size, int i, int j, int accumulator){
         int diamondRowThickness;
         if (accumulator <= 0){
